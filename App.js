c06ba3b68b6f1b8ba5a1888df485a86ca4ad9bc4
May <senaidev.may@gmail.com>
2025-08-27 17:08:24 -0300

    Update UI elements and styles for the counter app

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const handleReset = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Contador</Text>
+      <Text style={styles.title}>𐙚Contador!⋆ ࣪ ˖</Text>
       <Text style={styles.counterText}>{contador}</Text>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button} onPress={handleDecrement}>
@@ -29,11 +29,10 @@ const handleReset = () => {
         <TouchableOpacity style={styles.button} onPress={handleIncrement}>
           <Text  style={styles.buttonText}>Aumentar</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity style={[styles.button, styles.resetButton]} onPress={handleReset}>
-          <Text style={styles.buttonText} >Zerar</Text>
-        </TouchableOpacity>
       </View>
+      <TouchableOpacity style={[styles.button, styles.resetButton]} onPress={handleReset}>
+        <Text style={styles.buttonText} >Zerar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />        
     </View>
   );
@@ -42,38 +41,40 @@ const handleReset = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF3F1FF', // Cor de fundo principal
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title:{
-    fontSize:32,
+  title: {
+    fontSize: 32,
     fontWeight: "bold",
     marginBottom: 20,
+    color: "#D9A299", // Cor do título
   },
-  counterText:{
-    fontSize:64,
+  counterText: {
+    fontSize: 64,
     fontWeight: "bold",
-    color: "#333",
-    marginBottom:30
+    color: "#DCC5B2", // Cor do contador
+    marginBottom: 30,
   },
-  button:{
-    backgroundColor: "#007bff",
+  button: {
+    backgroundColor: "#D9A299", // Cor dos botões
     paddingVertical: 15,
     paddingHorizontal: 40,
-    borderRadius: 10,
+    borderRadius: 20, // Botões mais arredondados
   },
-  buttonText:{
-    color: "#fff",
+  buttonText: {
+    color: "#FAF7F3", // Cor do texto dos botões
     fontSize: 18,
     fontWeight: "bold",
   },
-  buttonContainer:{
+  buttonContainer: {
     flexDirection: "row",
-    marginBottom: 20,
+    marginBottom: 10, // Ajuste para espaçamento
     gap: 8,
   },
-  resetButton:{
-    backgroundColor: "#dc3545",
-  }
+  resetButton: {
+    backgroundColor: "#DCC5B2", // Cor do botão de reset
+    marginTop: 10, // Espaçamento acima do botão "Zerar"
+  },
 });
